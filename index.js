9eda2b12fd9ecc978d94275b2b9f7b54cb9fd0e4
Raksha Raut <raksharaut174@gmail.com>
2024-06-15 08:54:17 +0545

    HTML Document Render for /user req

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express =  require("express");
-// const users = require("./MOCK_DATA.json");
 const users = require("./MOCK_DATA.json"); 
 const app = express();
 const PORT = 8000;
 
 // // Routes
-app.get("/users", (req, res) =>{
+app.get("/users", (req,res) => {
+    const html =`
+    <ul> 
+    ${users.map((user) => `<li>${user.first_name}</li`).join("")}
+    </ul>
+    `;
+    res.send(html);
+});
+
+// REST API
+app.get("/api/users", (req, res) =>{
     return res.json(users);
 });
+
+
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
